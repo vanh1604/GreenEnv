5ebc72effa1649ea49d6abcbfd239baa64ef67ee
hoangquan456 <truonghoangquan456@gmail.com>
2023-07-24 20:14:12 +0700

    update

--- src/components/missions/missions2/MissionDetails.js
+++ src/components/missions/missions2/MissionDetails.js
@@ -10,48 +10,48 @@ import dot3 from "../img/Ellipse 5.svg";
 
 const MissionDetails = () => {
   return (
-    <main className="mission-details">
-      <div className="mission-header">
-        <div className="general-info">
-          <div className="header--first_line">
-            <div className="mission-title">Nhiệm vụ làm sạch</div>
-            <div className="mission-rewards">+50</div>
+    <main class="mission-details--mission-details">
+      <div class="mission-details--mission-header">
+        <div class="mission-details--general-info">
+          <div class="mission-details--header--first_line">
+            <div class="mission-details--mission-title">Nhiệm vụ làm sạch</div>
+            <div class="mission-details--mission-rewards">+50</div>
           </div>
-          <div className="header--second_line">
-            <div className="contact">
-              <img src={locationIcon} alt="" className="icon" />
-              <div className="mission-location">
+          <div class="mission-details--header--second_line">
+            <div class="mission-details--contact">
+              <img src={locationIcon} alt="" class="mission-details--icon" />
+              <div class="mission-details--mission-location">
                 Số 11, Vũ Phạm Hàm, Yên Hòa, Cầu Giấy, HN
               </div>
             </div>
 
-            <div className="contact">
-              <img src={callIcon} alt="" className="icon" />
-              <div className="mission-call">(+84) 912 345 678</div>
+            <div class="mission-details--contact">
+              <img src={callIcon} alt="" class="mission-details--icon" />
+              <div class="mission-details--mission-call">(+84) 912 345 678</div>
             </div>
           </div>
         </div>
-        <div className="button-container">
-          <button className="join-button">Tham gia</button>
+        <div class="mission-details--button-container">
+          <button class="mission-details--join-button">Tham gia</button>
         </div>
       </div>
-      <div className="mission-hero">
-        <img src={missionImg} alt="" className="mission-img" />
-        <div className="dots">
-          <img src={dot1} alt="" className="dot" />
-          <img src={dot2} alt="" className="dot" />
-          <img src={dot3} alt="" className="dot" />
+      <div class="mission-details--mission-hero">
+        <img src={missionImg} alt="" class="mission-details--mission-img" />
+        <div class="mission-details--dots">
+          <img src={dot1} alt="" class="mission-details--dot" />
+          <img src={dot2} alt="" class="mission-details--dot" />
+          <img src={dot3} alt="" class="mission-details--dot" />
         </div>
       </div>
-      <div className="mission-info">
-        <div className="mission-description">
+      <div class="mission-details--mission-info">
+        <div class="mission-details--mission-description">
           Lorem ipsum dolor sit amet consectetur. Eget nascetur in dolor lectus
           parturient pretium amet eu pellentesque. Senectus eu aliquet elementum
           id est. Ullamcorper ipsum in aliquet sed aliquam egestas diam. Egestas
           iaculis sit gravida fringilla egestas amet ac.
         </div>
-        <div className="mission-instruction">
-          <div className="title">Hướng dẫn:</div>
+        <div class="mission-details--mission-instruction">
+          <div class="mission-details--title">Hướng dẫn:</div>
           <ul>
             <li>Bước 1: Đăng ký nhận nhiệm vụ</li>
             <li>Bước 2: Thực hiện nhiệm vụ làm sạch</li>
@@ -62,15 +62,15 @@ const MissionDetails = () => {
             <li>Bước 5: Nhận điểm</li>
           </ul>
         </div>
-        <div className="mission-regulation">
-          <div className="title">Quy định:</div>
-          <div className="regulation-text">
+        <div class="mission-details--mission-regulation">
+          <div class="mission-details--title">Quy định:</div>
+          <div class="mission-details--regulation-text">
             Bạn cần phải hoàn thành nhiệm vụ trong vòng tối đa 3 ngày. Sau 3
             ngày nhiệm vụ sẽ tự động hủy
           </div>
         </div>
       </div>
-      <img src={map} alt="" className="map" />
+      <img src={map} alt="" class="mission-details--map" />
     </main>
   );
 };
